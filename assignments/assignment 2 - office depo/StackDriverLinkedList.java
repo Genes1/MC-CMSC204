364bda0d942464657003d05b52ff7a629177dea4
Eugene Domrachev <eug.domr@gmail.com>
2019-09-19 21:45:17 -0400

    Change A2 folder name, implemented array and linked list stacks

---
+++ assignments/assignment 2 - office depo/StackDriverLinkedList.java
@@ -0,0 +1,25 @@
+package com;
+
+public class StackDriverLinkedList {
+	
+	public static void main(String[] args) {
+		//StringStackArray s = new StringStackArray(3);
+		StringStackLinkedList<String> s = new StringStackLinkedList("a");	
+		s.push("b");
+		s.push("c");
+		s.push("d"); //expect error message here about stack space
+		s.display(); //expect a, b, c
+		System.out.println(s.pop() + "\n"); //expect c 
+		System.out.println(s.pop() + "\n"); //expect b
+		System.out.println(s.peek() + "\n"); //expect a
+		s.display(); //expect a, b
+		s.clear();
+		s.display(); //expect all null
+		s.push("yes"); 
+		s.push("no");
+		s.pop();
+		s.pop();
+		s.pop();
+	}
+	
+}
